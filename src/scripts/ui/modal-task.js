63d872db14d2c11084be639aa642a80eec80d24b
RidgeFoo <m.pagnol@pm.me>
2022-06-25 15:16:10 +0100

    Adds task properties to modal and finishes off the basics of the form

--- src/scripts/ui/modal-task.js
+++ src/scripts/ui/modal-task.js
@@ -7,36 +7,111 @@ export default (function () {
     modal.append(createTaskForm());
   }
 
-  function createTaskTitle() {
-    const container = document.createElement("div");
-    container.id = "container-task-title";
+  function createTaskInput() {
     const taskInput = document.createElement("input");
-    const taskLabel = document.createElement("label");
-
     taskInput.setAttribute("name", "taskTitle");
     taskInput.setAttribute("required", "");
     taskInput.setAttribute("type", "text");
     taskInput.setAttribute("placeHolder", "e.g. Take over the world!");
-    taskInput.id = "taskTitle";
+    taskInput.id = "task-title";
 
-    taskLabel.setAttribute("for", "taskTitle");
-    taskLabel.textContent = "Task Title: ";
-    container.append(taskLabel, taskInput);
-    return container;
+    return taskInput;
   }
 
   function createTaskProperties() {
     const container = document.createElement("div");
     container.id = "modal-task-properties";
 
-    const dueDate = document.createElement("input");
-    dueDate.id = "dueDate";
-    const dueDateLabel = document.createElement("label");
-    dueDateLabel.setAttribute("for", "dueDate");
-    dueDateLabel.textContent = "Due Date: ";
-    dueDate.setAttribute("type", "date");
+    container.append(
+      createDueDateInput(),
+      createPriorityDropDown(),
+      createProjectDropDown()
+    );
 
-    container.append(dueDate);
+    return container;
+  }
+
+  function createDueDateInput() {
+    const container = document.createElement("div");
+    container.id = "set-date";
+    container.className = "task-property";
+
+    const dueDate = document.createElement("input");
+    dueDate.id = "due-date";
+    dueDate.setAttribute("type", "date");
+    dueDate.setAttribute("name", "dueDate");
+    dueDate.setAttribute("required", "");
+
+    const dueDateLabel = document.createElement("label");
+    dueDateLabel.setAttribute("for", "due-date");
+    dueDateLabel.textContent = "Due Date";
+
+    container.append(dueDateLabel, dueDate);
+    return container;
+  }
+
+  function createPriorityDropDown() {
+    const container = document.createElement("div");
+    container.id = "set-priority";
+    container.className = "task-property";
+    const priorities = document.createElement("select");
+
+    priorities.id = "priority";
+    priorities.setAttribute("name", "priority");
+
+    const label = document.createElement("label");
+    label.setAttribute("for", "priority");
+    label.textContent = "Priority";
+
+    const options = ["Low", "Medium", "High"];
+
+    options.forEach((priority) => {
+      let option = document.createElement("option");
+      option.setAttribute("value", priority.toLowerCase());
+      option.textContent = priority;
+
+      priorities.appendChild(option);
+    });
+
+    container.append(label, priorities);
+
+    return container;
+  }
+
+  function createProjectDropDown() {
+    const projects = ["Inbox"];
+
+    // form data list element with options
+    // Have to allow the user to input a new project and create that project
+    const container = document.createElement("div");
+    container.id = "set-project";
+    container.className = "task-property";
+
+    const label = document.createElement("label");
+    label.textContent = "Project";
+    label.setAttribute("for", "project");
+
+    const dropDown = document.createElement("input");
+    dropDown.id = "project";
+    dropDown.setAttribute("name", "project");
+    dropDown.setAttribute("list", "projects");
+
+    // Set to our default project - TODO: Move the default creation out of here
+    dropDown.setAttribute("value", projects[0]);
+
+    const projectsList = document.createElement("datalist");
+    projectsList.id = "projects";
+
+    // Inbox will always be in the list and is the default
+    // Projects should be populated from local storage???
+
+    projects.forEach((proj) => {
+      const option = document.createElement("option");
+      option.setAttribute("value", proj);
+      projectsList.appendChild(option);
+    });
+
+    container.append(label, dropDown, projectsList);
 
     return container;
   }
@@ -49,8 +124,11 @@ export default (function () {
     btnAdd.setAttribute("type", "submit");
     const btnCancel = document.createElement("button");
     btnAdd.textContent = "Add Task";
+    btnAdd.id = "btn-add-task";
+    btnAdd.classList = "form-invalid";
     btnCancel.textContent = "Cancel";
     btnCancel.setAttribute("type", "reset");
+    btnCancel.id = "btn-cancel";
 
     btnCancel.addEventListener("click", toggleModal);
     container.append(btnCancel, btnAdd);
@@ -61,7 +139,9 @@ export default (function () {
   function createTaskForm() {
     const form = document.createElement("form");
 
-    form.append(createTaskTitle(), createTaskProperties(), createFormButtons());
+    form.addEventListener("change", () => toggleAddTaskButtonColour(form));
+
+    form.append(createTaskInput(), createTaskProperties(), createFormButtons());
 
     form.addEventListener("submit", (e) => {
       e.preventDefault();
@@ -69,6 +149,18 @@ export default (function () {
     });
 
     return form;
+  }
+
+  function toggleAddTaskButtonColour(form) {
+    const button = document.querySelector("#btn-add-task");
+    console.log("form check");
+
+    if (form.checkValidity()) {
+      button.classList.add("form-valid");
+    } else {
+      button.classList.remove("form-valid");
+      button.classList.add("form-invalid");
+    }
   }
 
   function toggleModal() {
